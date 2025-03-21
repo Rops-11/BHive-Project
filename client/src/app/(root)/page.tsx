58eb8bad0dd8a621413e80e8d9e4ffc5adc8573b
regbarte <reginetherese.barte@gmail.com>
2025-03-21 18:40:10 +0800

    Footer

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -1,11 +1,9 @@
-<<<<<<< HEAD:client/src/app/page.tsx
-import Carousel from "@/components/Carousel";
-=======
 import Carousel from "../../components/Carousel";
->>>>>>> b97d18e251781cd07aa76e61f76b63832a785475:client/src/app/(root)/page.tsx
+
 import BookRoom from "@/components/BookRoom";
 import LocationDescription from "@/components/LocationDescription";
 import ExploreFacilities from "@/components/ExploreFacilities";
+import Footer from "@/components/footer";
 export default function Home() {
   return (
     <main>
@@ -17,6 +15,7 @@ export default function Home() {
       <div className="container mx-auto px-4 py-8">
       <ExploreFacilities />
       </div>
+      < Footer/>
   
     </main>
   );
