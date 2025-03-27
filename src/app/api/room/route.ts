66724720f727ae82f45982b85a742a2f9a344514
nickasss <nickasss>
2025-03-27 19:57:27 +0800

    added room and bookings route

---
+++ src/app/api/room/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const rooms = await prisma.room.findMany({
+      include: { bookings: true },
+    });
+    return NextResponse.json(rooms, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch rooms" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const { roomType, roomNumber, isAvailable, maxGuests, roomRate } =
+      await req.json();
+
+    const newRoom = await prisma.room.create({
+      data: { roomType, roomNumber, isAvailable, maxGuests, roomRate },
+    });
+
+    return NextResponse.json(newRoom, { status: 201 });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { error: "Failed to create room" },
+      { status: 500 }
+    );
+  }
+}
