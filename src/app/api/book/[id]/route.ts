0edeb999a9c07503650287e2f43fdd3ca337bdc0
Rops-11 <jrofer.casio11@gmail.com>
2025-04-19 17:13:44 +0800

    Update route.ts

---
+++ src/app/api/book/[id]/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "utils/db";
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+
+    await prisma.booking.delete({
+      where: { id },
+    });
+
+    return NextResponse.json({ message: "Booking deleted" }, { status: 200 });
+  } catch (error: unknown) {
+    console.error("Error deleting room:", error);
+
+    const errorMessage =
+      error instanceof Error ? error.message : "Unknown error occurred";
+
+    const errorStack =
+      error instanceof Error && process.env.NODE_ENV === "development"
+        ? error.stack
+        : undefined;
+
+    return NextResponse.json(
+      {
+        error: "Failed to delete room",
+        message: errorMessage,
+        stack: errorStack,
+      },
+      { status: 500 }
+    );
+  }
+}
