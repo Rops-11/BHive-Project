74918a6537eff97cde9d72513b722321895ded3b
Rops-11 <jrofer.casio11@gmail.com>
2025-05-08 13:23:13 +0800

    Admin Rooms
    Main Edited Files:
    HotelRoomCard.tsx
    admin/rooms/page.tsx

---
+++ src/components/Room/HotelRoom.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import HotelRoom from "@/components/Room/HotelRoomCard";
+
+const meta = {
+  title: "RoomTour/HotelRoom",
+  component: HotelRoom,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "fullscreen",
+  },
+} satisfies Meta<typeof HotelRoom>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
