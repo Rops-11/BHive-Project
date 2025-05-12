d61af869f457fcfc8a8d8dffbbf132333db39b30
regbarte <reginetherese.barte@gmail.com>
2025-05-12 19:40:59 +0800

    Paymongo link

--- src/components/Payment/Invoice.tsx
+++ src/components/Payment/Invoice.tsx
@@ -56,9 +56,55 @@ const InvoiceCard = ({
   const totalRoomPrice = roomData!.roomRate! * daysDiff!;
   const total = excessGuestPrice + totalRoomPrice;
 
-  const handleClickPay = () => {
+  const handleClickPay = async() => {
     setBookingContext!({ ...bookingContext, totalPrice: total });
-    router.push("/book/payment"); 
+    // router.push("/book/payment"); 
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+        'Content-Type': 'application/json',
+        authorization: 'Basic c2tfdGVzdF9qV25KV2NhM0U1ZjIycUVGam0yRnZ5WGc6cmVnaW5lTmJhcnRlMDEwNTA1'
+      },
+      body: JSON.stringify({
+        data: {
+          attributes: {
+            send_email_receipt: false,
+            show_description: true,
+            show_line_items: true,
+            line_items: [
+              {
+                currency: 'PHP',
+                amount: 1890000,
+                description: 'QUEEN BEE 2A',
+                name: 'QUEEN BEE 2A',
+                quantity: 1
+              }
+            ],
+            payment_method_types: ['gcash', 'brankas_bdo'],
+            description: 'queen room'
+          }
+        }
+      })
+    };
+    
+    const response = await fetch('https://api.paymongo.com/v1/checkout_sessions', options) 
+    const data = await response.json();
+    console.log(data);
+    
+    
+    if (response.ok) {
+      const sessionId = data.data.id;
+      const checkoutUrl = data.data.attributes.checkout_url;
+
+      router.push(checkoutUrl);
+
+
+    } else {
+
+      console.error("Payment error:", data);
+    }
+
   };
 
   return (
