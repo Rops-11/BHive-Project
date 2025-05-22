3405d7598b923fb361f2d68a26240e1631a8ba1a
Rops-11 <jrofer.casio11@gmail.com>
2025-05-22 16:43:40 +0800

    Update popover.tsx

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -70,6 +70,8 @@ const PopoverContent = React.forwardRef<
   }
 );
 
+PopoverContent.displayName = PopoverPrimitive.Content.displayName;
+
 function PopoverAnchor({
   ...props
 }: React.ComponentProps<typeof PopoverPrimitive.Anchor>) {
